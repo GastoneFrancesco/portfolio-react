96db924faea6dec7735fff8198f1bdcb46ddcee9
Francesco Gastone <70978571+GastoneFrancesco@users.noreply.github.com>
2023-04-20 22:51:34 +0200

    Add _redirects file for Netlifly - Add auto redirect to homePage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
 import { HomePageComponent } from './component/HomePageComponent';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -11,11 +11,12 @@ library.add(fab, faCheckSquare, faCoffee)
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route exact path="/homePage" element={<HomePageComponent />} /> 
-      <Route exact path="/work" element={<WorkComponent />} /> 
-    </Routes>
-  </Router>
+      <Routes>
+        <Route exact path="/" element={<Navigate to="/homePage" />} />
+        <Route exact path="/homePage" element={<HomePageComponent />} />
+        <Route exact path="/work" element={<WorkComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
